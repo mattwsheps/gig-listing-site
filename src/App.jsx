524e4bc02d9a30d61ccbffae0218226bb6e5382e
Matthew Gentle-Shepherd <matthew.gentle@gmail.com>
2024-01-23 15:36:59 +0000

    Added props

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,33 @@ import Gig from "./components/Gig.jsx";
 import "./App.css";
 
 function App() {
-  return (
-    <>
-      <Gig />
-    </>
-  );
+    return (
+        <>
+            <Gig
+                name="Parcels"
+                image="src/assets/parcels.jpg"
+                description="Welcome to an unforgettable night of music with Parcels, the
+                            sensational band that combines groovy rhythms with soulful
+                            melodies! Join us for a mesmerizing live concert experience that
+                            will leave you dancing and singing along."
+                datetime="7pm 21/02/2024"
+                location="Berlin, Germany"
+            />
+
+            <Gig
+                name="Jungle"
+                image="src/assets/jungle.jpg"
+                description="Get ready for a night of unparalleled excitement with Jungle, 
+                            the dynamic ensemble that effortlessly blends funk, soul, and 
+                            electronic vibes to create a sonic journey like no other. This 
+                            highly acclaimed band is set to transform your event into a Jungle 
+                            Groove Extravaganza, leaving your audience mesmerized and craving 
+                            for more."
+                datetime="8pm 23/02/2024"
+                location="London, UK"
+            />
+        </>
+    );
 }
 
 export default App;
--- src/components/Gig.jsx
+++ src/components/Gig.jsx
@@ -1,19 +1,14 @@
 import React from "react";
-import "./Gig.css"
+import "./Gig.css";
 
-const Gig = () => {
+const Gig = (props) => {
     return (
         <div className="gig">
-            <h3 className="band-name">Parcels</h3>
-            <img className="band-image" src="src/assets/parcels.jpg" />
-            <p className="description">
-                Welcome to an unforgettable night of music with Parcels, the
-                sensational band that combines groovy rhythms with soulful
-                melodies! Join us for a mesmerizing live concert experience that
-                will leave you dancing and singing along.
-            </p>
-            <p className="date-time">7pm 21/02/2024</p>
-            <p className="location">Berlin</p>
+            <h3 className="band-name">{props.name}</h3>
+            <img className="band-image" src={props.image} />
+            <p className="description">{props.description}</p>
+            <p className="date-time">{props.datetime}</p>
+            <p className="location">{props.location}</p>
         </div>
     );
 };
